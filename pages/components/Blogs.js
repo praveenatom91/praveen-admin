60ec4e5944a2d4478bf5ffda893edecb2c61a221
akku750156 <akashtiwari750156@gmail.com>
2022-03-04 19:42:06 +0530

    Added tinymce

--- pages/components/Blogs.js
+++ pages/components/Blogs.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
+import { Editor } from "@tinymce/tinymce-react";
+import slugify from "react-slugify";
+
 toast.configure();
 
 function Blogs() {
@@ -27,6 +30,7 @@ function Blogs() {
             image: url,
             title: title,
             content: content,
+            slug: slugify(title),
           })
           .then((res) => {
             console.log(res);
@@ -41,7 +45,7 @@ function Blogs() {
   return (
     <div className="h-screen">
       <div className="h-full flex flex-col justify-center items-center ">
-        <div className="h-[50%] flex flex-col  justify-around items-center drop-shadow-gray-600 drop-shadow-2xl bg-gray-200 p-4">
+        <div className="flex flex-col  justify-around items-center drop-shadow-gray-600 drop-shadow-2xl bg-gray-200 p-4">
           <input
             type="file"
             name="photo"
@@ -58,17 +62,33 @@ function Blogs() {
             className="w-96 outline-none p-2 m-2"
             placeholder="Title..."
           />
-          <textarea
-            type="text"
-            name="content"
-            onChange={(e) => setContent(e.target.value)}
-            className="w-96 h-48 outline-none p-4 resize-none"
-            placeholder="Have something to write ...."
+
+          <Editor
+            textareaName="content"
+            initialValue="Write your content here...."
+            className="w-10/12"
+            onEditorChange={(newText) => setContent(newText)}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+              ],
+              toolbar:
+                "undo redo | formatselect | " +
+                "bold italic backcolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
           />
           <button
             type="submit"
             onClick={uploadBlog}
-            className="py-2 px-4 shadow-2xl bg-gray-300 rounded-3xl"
+            className="py-2 px-4 shadow-2xl bg-gray-300 rounded-3xl "
           >
             Submit
           </button>
